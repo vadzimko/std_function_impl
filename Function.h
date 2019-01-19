e9da5f1b677db1847ae72be1337d430b8f381b99
Vadim Badyaev <vvadzimko@gmail.com>
2019-01-19 17:00:08 +0300

    Add files via upload

---
+++ Function.h
@@ -0,0 +1,144 @@
+#ifndef FUNC_FUNCTION_H
+#define FUNC_FUNCTION_H
+
+#include <cstddef>
+#include <iostream>
+#include <array>
+
+template <class R, class ...Args>
+struct callable_base {
+    virtual R operator()(Args... args) const = 0;
+    virtual ~callable_base() = default;
+    virtual callable_base *clone() const = 0;
+};
+
+template <class F, class R, class ...Args>
+struct callable : callable_base<R, Args...> {
+    explicit callable(F const& functor) : functor(functor) {}
+    R operator()(Args... args) const { return functor(args...); }
+    callable *clone() const { return new callable(functor); }
+
+private:
+    F functor;
+};
+
+template <class Func>
+struct func_type_getter {
+    typedef Func type;
+};
+
+template <class Result,class ...Args>
+struct func_type_getter<Result(Args...)>
+{
+    typedef Result (*type)(Args...);
+};
+
+template <class> class Function;
+
+template <class R, class ...Args>
+struct Function<R(Args...)> {
+    typedef R result_type;
+
+    Function() noexcept;
+    explicit Function(std::nullptr_t) noexcept;
+    Function(Function const& rhs);
+    Function(Function&& other) noexcept;
+
+    template<typename F>
+    Function(F const& func);
+
+    ~Function();
+
+    Function& operator=(const Function& other);
+    Function& operator=(Function&& other) noexcept;
+
+    void swap(Function& other) noexcept;
+
+    explicit operator bool() const noexcept;
+    R operator()(Args...) const;
+
+private:
+    callable_base<R,Args...> *f = nullptr;
+    static unsigned const BUF_MAX_SIZE = 1024;
+    char buffer[BUF_MAX_SIZE] = {};
+    bool is_small = false;
+};
+
+template <class R, class ...Args>
+Function<R(Args...)>::Function() noexcept = default;
+
+template <class R, class ...Args>
+Function <R(Args...)>::Function(std::nullptr_t) noexcept {}
+
+template <class R, class ...Args>
+R Function<R(Args...)>::operator()(Args... args) const {
+    assert(*this);
+
+    if (is_small) {
+        return (*static_cast<callable_base<R, Args...> const *>(static_cast<void const*>((buffer))))(args...);
+    } else {
+        return (*f)(args...);
+    }
+}
+
+template <class R, class ...Args>
+template <class F>
+Function<R(Args...)>::Function(F const& func) {
+    if (sizeof(func) < BUF_MAX_SIZE) {
+        new (buffer) callable<typename func_type_getter<F>::type, R, Args...>(func);
+        is_small = true;
+    } else {
+        f = new callable<typename func_type_getter<F>::type, R, Args...>(func);
+        is_small = false;
+    }
+}
+
+template <class R, class ...Args>
+Function <R(Args...)>::Function(Function const& rhs) {
+    if (rhs) {
+        is_small = rhs.is_small;
+        if (rhs.is_small) {
+            f = rhs.f->clone();
+        } else {
+            memcpy(buffer, rhs.buffer, BUF_MAX_SIZE);
+        }
+    }
+}
+
+template <class R, class ...Args>
+Function<R(Args...)>::operator bool() const noexcept {
+    return (is_small || f != nullptr);
+}
+
+template <class R, class ...Args>
+Function<R(Args...)>::~Function() {
+    delete f;
+}
+
+template<class R, class... Args>
+Function<R(Args...)>::Function(Function &&other) noexcept : Function() {
+    swap(other);
+}
+
+template<class R, class... Args>
+void Function<R(Args...)>::swap(Function &other) noexcept {
+    std::swap(is_small, other.is_small);
+    std::swap(f, other.f);
+    std::swap_ranges(buffer, buffer + BUF_MAX_SIZE, other.buffer);
+}
+
+template<class R, class... Args>
+Function<R(Args...)>& Function<R(Args...)>::operator=(const Function &other) {
+    if (this != &other) {
+        Function<R(Args...)> temp(other);
+        swap(temp);
+    }
+    return *this;
+}
+
+template<class R, class... Args>
+Function<R(Args...)>& Function<R(Args...)>::operator=(Function &&other) noexcept {
+    swap(other);
+    return *this;
+}
+#endif //FUNC_FUNCTION_H
---
+++ main.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include "Function.h"
+
+using namespace std;
+
+string many_a(int a) {
+    string res;
+    for (int i = 0; i < a; i++) {
+        res += 'a';
+    }
+
+    return res;
+}
+
+void test_function() {
+    Function<string(int)> function(&many_a);
+
+    assert(function(3) == "aaa");
+}
+
+void test_function_assignment() {
+    Function<string(int)> function = &many_a;
+
+    assert(function(4) == "aaaa");
+}
+
+void test_lambda() {
+    Function<int()> function([](){ return 1;});
+
+    assert(function() == 1);
+}
+
+void test_lambda_byref() {
+    int a = 1;
+    Function<int()> function([&](){ return a++;});
+
+    assert(function() == 1);
+    assert(function() == 2);
+    assert(function() == 3);
+    assert(a == 4);
+}
+
+void test_lambda_byval() {
+    int a = 1;
+    Function<int()> function([=](){ return a;});
+    a++;
+
+    assert(function() == 1);
+    assert(a == 2);
+}
+
+void test_lambda_assignment() {
+    Function<int()> function = [](){ return 2;};
+
+    assert(function() == 2);
+}
+
+void test_empty() {
+    Function<void()> a;
+
+    assert(!a);
+}
+
+void test_copy_empty() {
+    Function<void()> a([](){int t = 1;});
+
+    assert(a);
+
+    Function<void()> b;
+    assert(!b);
+    a = b;
+
+    assert(!a);
+}
+
+void test_swap() {
+    Function<int()> a([](){return 1;});
+    Function<int()> b([](){return 2;});
+
+    a.swap(b);
+
+    assert(a() == 2);
+    assert(b() == 1);
+}
+
+void test_move() {
+    Function<int()> a([](){return 1;});
+    Function<int()> b(move(a));
+
+    assert(b() == 1);
+}
+
+void test_move_assignment() {
+    Function<int()> a([](){return 1;});
+    Function<int()> b([](){return 2;});
+
+    a = move(b);
+    assert(a() == 2);
+}
+
+int main() {
+    test_function();
+    test_function_assignment();
+
+    test_lambda();
+    test_lambda_byref();
+    test_lambda_byval();
+    test_lambda_assignment();
+
+    test_empty();
+    test_copy_empty();
+    test_swap();
+
+    test_move();
+    test_move_assignment();
+
+    cout << "TESTS OK";
+    return 0;
+}
