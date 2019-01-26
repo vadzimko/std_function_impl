7d36d2c09bc7b709f5ebcefb44cdd8501dd2f8ed
v.badyaev <v.badyaev@corp.vk.com>
2019-01-26 04:06:39 +0300

    union created

--- Function.h
+++ Function.h
@@ -6,23 +6,7 @@
 #include <array>
 #include <cassert>
 #include <cstring>
-
-template <class R, class ...Args>
-struct callable_base {
-    virtual R operator()(Args... args)  = 0;
-    virtual ~callable_base() = default;
-    virtual callable_base *clone()  = 0;
-};
-
-template <class F, class R, class ...Args>
-struct callable : callable_base<R, Args...> {
-    explicit callable(F& functor) : functor(std::move(functor)) {}
-    R operator()(Args... args)  { return functor(std::forward<Args>(args)...); }
-    callable *clone() { return new callable(functor); }
-
-private:
-    F functor;
-};
+#include <memory>
 
 template <class Func>
 struct func_type_getter {
@@ -39,110 +23,105 @@ template <class> class Function;
 
 template <class R, class ...Args>
 struct Function<R(Args...)> {
-    typedef R result_type;
 
-    Function() noexcept;
-    explicit Function(std::nullptr_t) noexcept;
-    Function(Function const& rhs);
-    Function(Function&& other) noexcept;
+    struct callable_base {
+        virtual R operator()(Args... args)  = 0;
+        virtual ~callable_base() = default;
+        virtual callable_base *clone()  = 0;
+        virtual void placement_clone(void* buff) = 0;
+    };
+
+    template <typename F>
+    struct callable : callable_base {
+        explicit callable(F& functor) : functor(std::move(functor)) {}
+        R operator()(Args... args)  { return functor(std::forward<Args>(args)...); }
+        callable *clone() { return new callable(functor); }
+        void placement_clone(void* buff) { new (buff) callable<F>(functor); }
+
+    private:
+        F functor;
+    };
+
+    Function() noexcept : f(nullptr) {}
+    explicit Function(std::nullptr_t) noexcept {}
+    Function(Function const& rhs) {
+        *this = rhs;
+    }
+
+    Function(Function&& other) noexcept {
+        swap(other);
+    }
 
     template<typename F>
-    Function(F func);
-
-    ~Function();
-
-    Function& operator=(const Function& other);
-    Function& operator=(Function&& other) noexcept;
-
-    void swap(Function& other) noexcept;
-
-    explicit operator bool() const noexcept;
-    R operator()(Args...) const;
-
-private:
-    callable_base<R,Args...> *f = nullptr;
-    static unsigned const BUF_MAX_SIZE = 1024;
-    char buffer[BUF_MAX_SIZE] = {};
-    bool is_small = false;
-};
-
-template <class R, class ...Args>
-Function<R(Args...)>::Function() noexcept = default;
-
-template <class R, class ...Args>
-Function <R(Args...)>::Function(std::nullptr_t) noexcept {}
-
-template <class R, class ...Args>
-R Function<R(Args...)>::operator()(Args... args) const {
-    assert(*this);
-
-    if (is_small) {
-        return (*(callable_base<R, Args...> *)(buffer))(args ...);
-    } else {
-        return (*f)(args...);
-    }
-}
-
-template <class R, class ...Args>
-template <class F>
-Function<R(Args...)>::Function(F func) {
-    if (sizeof(func) < BUF_MAX_SIZE) {
-        new (buffer) callable<typename func_type_getter<F>::type, R, Args...>(func);
-        is_small = true;
-    } else {
-        f = new callable<typename func_type_getter<F>::type, R, Args...>(func);
-        is_small = false;
-    }
-}
-
-template <class R, class ...Args>
-Function <R(Args...)>::Function(Function const& rhs) {
-    if (rhs) {
-        is_small = rhs.is_small;
-        if (rhs.is_small) {
-            memcpy(buffer, rhs.buffer, BUF_MAX_SIZE);
+    Function(F func) {
+        if (sizeof(func) < BUF_MAX_SIZE) {
+            new (buffer) callable<typename func_type_getter<F>::type>(func);
+            is_small = true;
         } else {
-            f = rhs.f->clone();
+            f = new callable<typename func_type_getter<F>::type>(func);
+            is_small = false;
+        }
+        assigned = true;
+    }
+
+    ~Function() {
+        if (!assigned) {
+            return;
+        }
+
+        if (!is_small) {
+            delete f;
+        } else {
+            ((callable_base*)buffer)->~callable_base();
         }
     }
-}
 
-template <class R, class ...Args>
-Function<R(Args...)>::operator bool() const noexcept {
-    return (is_small || f != nullptr);
-}
-
-template <class R, class ...Args>
-Function<R(Args...)>::~Function() {
-    if (!is_small) {
-        delete f;
+    Function& operator=(const Function& other) {
+        is_small = other.is_small;
+        assigned = other.assigned;
+        if (assigned) {
+            if (other.is_small) {
+                auto func = (callable_base*)(other.buffer);
+                func->placement_clone(buffer);
+            } else {
+                f = other.f->clone();
+            }
+        }
+        return *this;
     }
-}
 
-template<class R, class... Args>
-Function<R(Args...)>::Function(Function &&other) noexcept : Function() {
-    swap(other);
-}
-
-template<class R, class... Args>
-void Function<R(Args...)>::swap(Function &other) noexcept {
-    std::swap(is_small, other.is_small);
-    std::swap(f, other.f);
-    std::swap_ranges(buffer, buffer + BUF_MAX_SIZE, other.buffer);
-}
-
-template<class R, class... Args>
-Function<R(Args...)>& Function<R(Args...)>::operator=(const Function &other) {
-    if (this != &other) {
-        Function<R(Args...)> temp(other);
-        swap(temp);
+    Function& operator=(Function&& other) noexcept {
+        swap(other);
+        return *this;
     }
-    return *this;
-}
 
-template<class R, class... Args>
-Function<R(Args...)>& Function<R(Args...)>::operator=(Function &&other) noexcept {
-    swap(other);
-    return *this;
-}
+    void swap(Function& other) noexcept {
+        std::swap(is_small, other.is_small);
+        std::swap(assigned, other.assigned);
+        std::swap(buffer, other.buffer);
+    }
+
+    explicit operator bool() const noexcept {
+        return assigned;
+    }
+    R operator()(Args... args) const {
+        assert(*this);
+
+        if (is_small) {
+            return (*(callable_base*)(buffer))(std::forward<Args>(args)...);
+        } else {
+            return (*f)(args...);
+        }
+    }
+
+private:
+    static unsigned const BUF_MAX_SIZE = 1024;
+    bool is_small = false;
+    bool assigned = false;
+
+    union {
+        callable_base *f = nullptr;
+        char buffer[BUF_MAX_SIZE];
+    };
+};
 #endif //FUNC_FUNCTION_H
